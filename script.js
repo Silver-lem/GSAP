91aff1653af310a597deae3838a0a8d4fbff046f
Geethika <april.appy24@gmail.com>
2026-01-12 05:26:46 +0530

    to and from

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.to("#box1",{
+    x:1200,
+    duration:2,
+    delay:1 
+})
+
+// gsap.to("#box2",{
+//     x:500,
+//     y:500,
+//     duration:2,
+//     delay:1 
+// })
+
+gsap.from("#box2",{
+    x:1200,
+    duration:2,
+    delay:1 
+})
