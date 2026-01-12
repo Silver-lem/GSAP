4607287d5e554c9f79f6cc246e49740c5bbcb6f9
Geethika <april.appy24@gmail.com>
2026-01-12 18:55:59 +0530

    static triigers

--- ScrollTrigger/script.js
+++ ScrollTrigger/script.js
@@ -8,14 +8,17 @@ gsap.from("#page1 #box",{
 gsap.from("#page2 #box",{
     scale:0,
     // delay:1,
-    duration:2,
-    rotate:360,
+    // duration:2,
+    duration:1,
+    opacity:0,
+    rotate:720,
     // scrollTrigger:"#page2 #box"
     scrollTrigger:{
         trigger: "#page2 #box",
         scroller:"body",
         markers:true,
-        start:"top 60%"
+        start:"top 60%",
+        end:"top 10%" 
     }
 
 })
