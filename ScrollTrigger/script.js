926a007d817161ef006e4a631f77bc61066a5ce5
Geethika <april.appy24@gmail.com>
2026-01-12 18:36:52 +0530

    scrollTrigger

---
+++ ScrollTrigger/script.js
@@ -0,0 +1,35 @@
+gsap.from("#page1 #box",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:360,
+})
+
+gsap.from("#page2 #box",{
+    scale:0,
+    // delay:1,
+    duration:2,
+    rotate:360,
+    // scrollTrigger:"#page2 #box"
+    scrollTrigger:{
+        trigger: "#page2 #box",
+        scroller:"body",
+        markers:true,
+        start:"top 60%"
+    }
+
+})
+
+gsap.from("#page3 #box",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:360,
+    // scrollTrigger:"#page3 #box"
+    scrollTrigger:{
+        trigger: "#page3 #box",
+        scroller:"body",
+        markers:true,
+        start:"top 60%"
+    }
+})
