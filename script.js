6e3377dbcf3b438c1239a3277b65062d45133d42
Geethika <april.appy24@gmail.com>
2026-01-12 05:46:34 +0530

    tetx basics

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-gsap.to("#box1",{
-    x:1200,
-    duration:2,
-    delay:1 
-})
+// gsap.to("#box1",{
+//     x:1200,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor: "blue",
+//     borderRadius:"50%",
+//     scale:0.5,
+// })
 
 // gsap.to("#box2",{
 //     x:500,
@@ -11,8 +15,17 @@ gsap.to("#box1",{
 //     delay:1 
 // })
 
-gsap.from("#box2",{
-    x:1200,
+// gsap.from("#box2",{
+//     x:1200,
+//     duration:2,
+//     delay:1 
+// })
+
+gsap.from("h1",{
+    // color:"crimson",
+    opacity:0,
     duration:2,
-    delay:1 
+    y:30,
+    delay:1,
+    stagger: 0.3,
 })
