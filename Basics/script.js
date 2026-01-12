9bdc0d2dcae648fc2934a24f94c61a6622fec52e
Geethika <april.appy24@gmail.com>
2026-01-12 06:35:17 +0530

    Simple Nav animated

--- Basics/script.js
+++ Basics/script.js
@@ -42,24 +42,48 @@
 // })
 
 //Second Part------------------------------------
-gsap.to("#box1",{
-    x:1500,
+// gsap.to("#box1",{
+//     x:1500,
+//     rotate:360,
+//     duration:1.5,
+//     delay:1,
+// })
+
+// gsap.to("#box2",{
+//     x:1500,
+//     backgroundColor:"yellow",
+//     duration:1.5,
+//     delay:2.5,
+// })
+
+// gsap.to("#box",{
+//     x:1500,
+//     scale:0.5,
+//     borderRadius:"50%",
+//     duration:1.5,
+//     delay:4,
+// })
+
+var tl = gsap.timeline()
+
+tl.to("#box1",{
+     x:1500,
     rotate:360,
     duration:1.5,
     delay:1,
 })
 
-gsap.to("#box2",{
+tl.to("box2",{
     x:1500,
-    backgroundColor:"yellow",
+    // backgroundColor:"yellow",
     duration:1.5,
-    delay:2.5,
+    // delay:2.5,
 })
 
-gsap.to("#box",{
-    x:1500,
-    scale:0.5,
-    borderRadius:"50%",
+tl.to("box",{
+     x:1500,
+    // scale:0.5,
+    // borderRadius:"50%",
     duration:1.5,
-    delay:4,
+    // delay:4,
 })
